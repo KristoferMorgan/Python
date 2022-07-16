2273db14b255c4d491415fd0f1e6f0dd81848df1
Christopher <rezielsaw@gmail.com>
2022-07-16 18:03:41 +1000

    Python HomeWork002 Task005

--- семинары/Lesson_2/HomeWork/Task005.py
+++ семинары/Lesson_2/HomeWork/Task005.py
@@ -1,1 +1,12 @@
 # Реализуйте алгоритм перемешивания списка
+import random
+
+def shuffle(my_list:list):
+    my_list = list(my_list)
+    random.shuffle(my_list)
+    return my_list
+
+your_list = [i for i in input("enter element list:").split()]
+new_list = shuffle(your_list)
+print (f"it's your list: {your_list}")
+print (f"after mixing the elements, we get: {new_list}")
