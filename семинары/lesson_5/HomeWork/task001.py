c2d0bdd14273139f5ee07c6c2002ec4849c52cc4
Кристофер Морган <rezielsaw@gmail.com>
2022-07-28 22:01:28 +1000

    A byte of python

--- семинары/lesson_5/HomeWork/task001.py
+++ семинары/lesson_5/HomeWork/task001.py
@@ -4,3 +4,21 @@ word = 'абв фгбв абв Ура,питон круто,очень инте�
 
 my_str = word.replace("абв","")
 print(my_str)
+
+
+----------------------------------or--------------------------------------
+
+
+import os
+
+path = os.path.join(r'Task_files','task001.txt')
+with open(path,'r') as data:
+    my_list = data.readline()
+
+def f(my_list:list):
+    for i in range(1 , len(my_list)):
+        if my_list[i] - my_list[i - 1] != 1:
+            return my_list[i - 1] + 1
+    return 'Not found'
+x = tuple(filter(lambda  i: (my_list[i-1]))) != 1
+print(x[0] + 1)
